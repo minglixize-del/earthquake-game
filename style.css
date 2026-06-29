27d8b0ea64c1b0b2a023e9c69dcd0aa01498e994
minglixize-del <minglixize@gmail.com>
2026-06-29 03:59:20 +0000

    初めての防災ゲーム

---
+++ style.css
@@ -0,0 +1,21 @@
+body{
+font-family:sans-serif;
+text-align:center;
+background:#f5f5f5;
+}
+
+#map{
+width:90%;
+height:500px;
+margin:auto;
+border-radius:10px;
+}
+
+button{
+margin-top:20px;
+padding:15px 30px;
+font-size:20px;
+border:none;
+border-radius:10px;
+cursor:pointer;
+}
