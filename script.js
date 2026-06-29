9b93081e6cb77cdf520d84e33c7eb05c2425ba37
minglixize-del <minglixize@gmail.com>
2026-06-29 06:57:19 +0000

    初めての防災ゲーム改

--- script.js
+++ script.js
@@ -1,21 +1,42 @@
-body{
-font-family:sans-serif;
-text-align:center;
-background:#f5f5f5;
-}
+// 東御市を表示
+const map = L.map('map').setView([36.36,138.33],13);
 
-#map{
-width:90%;
-height:500px;
-margin:auto;
-border-radius:10px;
-}
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
+attribution:'© OpenStreetMap'
+}).addTo(map);
 
-button{
-margin-top:20px;
-padding:15px 30px;
-font-size:20px;
-border:none;
-border-radius:10px;
-cursor:pointer;
+// 家
+const home=L.marker([36.36,138.33]).addTo(map);
+home.bindPopup("🏠 あなたの家");
+
+// 避難所
+const shelter=L.marker([36.37,138.34]).addTo(map);
+shelter.bindPopup("🏫 避難所");
+
+// 避難ルート
+let route=L.polyline([
+[36.36,138.33],
+[36.37,138.34]
+],{
+color:"blue",
+weight:5
+}).addTo(map);
+
+// 地震
+function earthquake(){
+
+route.setStyle({
+color:"red",
+dashArray:"10"
+});
+
+const events=[
+"橋が崩落しました！",
+"道路が通行止めになりました！",
+"土砂崩れが発生しました！"
+];
+
+const event=events[Math.floor(Math.random()*events.length)];
+
+alert("🚨 "+event+"\n新しい避難ルートを考えてください！");
 }
