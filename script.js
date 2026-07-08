a4b3d259a9f617e321edcd8861be0229b96bbbb8
minglixize-del <minglixize@gmail.com>
2026-07-08 11:14:58 +0000

    赤線の距離

--- script.js
+++ script.js
@@ -215,36 +215,47 @@ drawRoadBlock(home, shelter);
 
 function drawRoadBlock(home, shelter){
 
-// 家と避難所の中間地点
-const midLat = (home.lat + shelter.lat) / 2;
-const midLng = (home.lng + shelter.lng) / 2;
-
 // 家→避難所の向き
 const dx = shelter.lng - home.lng;
 const dy = shelter.lat - home.lat;
 
-// 長さ
+// 距離
 const length = Math.sqrt(dx * dx + dy * dy);
 
 // 長さが0なら終了
 if(length === 0) return;
 
+// 家から何％の位置に置くか
+let ratio = 0.25;
+
+// 約100m（緯度経度の近似）
+const maxDistance = 0.0009;
+
+// 25%地点が100mより遠いなら100m地点にする
+if(length * ratio > maxDistance){
+ratio = maxDistance / length;
+}
+
+// 通行止めを置く位置
+const blockLat = home.lat + dy * ratio;
+const blockLng = home.lng + dx * ratio;
+
 // ルートと垂直方向のベクトル
 const px = -dy / length;
 const py = dx / length;
 
-// 赤線の長さ（あとで調整できる）
+// 赤線の長さ
 const size = 0.0006;
 
 // 赤線の両端
 const p1 = [
-midLat + py * size,
-midLng + px * size
+blockLat + py * size,
+blockLng + px * size
 ];
 
 const p2 = [
-midLat - py * size,
-midLng - px * size
+blockLat - py * size,
+blockLng - px * size
 ];
 
 const block = L.polyline(
